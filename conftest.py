4e4a1500d512118995dcf5e0893788d5bb6d43ff
Trukhin_Evgeniy <tev1992@yandex.ru>
2025-03-16 15:25:49 +0500

    правки

--- conftest.py
+++ conftest.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
 
+language = ["en", "fr", "es", "ru"]
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None, help='Выбери локализацию тестов: "--language=en" or "--language=ru"')
 
-@pytest.fixture(scope='class')
+@pytest.fixture(params=language)
 def browser(request):
     user_language = request.config.getoption('language')
-    browser = None
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_language': user_language})
     browser = webdriver.Chrome(options=options)
--- test_items.py
+++ test_items.py
@@ -3,12 +3,14 @@ import time
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
+from conftest import language
+
+
 class TestElementDisplayed():
     def test_items(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         browser.get(link)
         time.sleep(5)
-
         try:
             button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
             if button.is_displayed():
