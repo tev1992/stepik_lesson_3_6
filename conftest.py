052b10d882a6b668df50501919c3d84680a5d645
Trukhin_Evgeniy <tev1992@yandex.ru>
2025-03-22 23:49:48 +0500

    правки

--- conftest.py
+++ conftest.py
@@ -1,18 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 import pytest
-
-language = ["en", "fr", "es", "ru"]
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None, help='Выбери локализацию тестов: "--language=en" or "--language=ru"')
+    parser.addoption('--browser_name', action='store', default="Chrome", help='Выбери браузер: "--browser_name=chrome" or "--browser_name=firefox"')
 
-@pytest.fixture(params=language)
+@pytest.fixture(scope='function')
 def browser(request):
     user_language = request.config.getoption('language')
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_language': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption('browser_name')
+
+    # Настройка браузера
+    if browser_name == 'Chrome':
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'Firefox':
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference('intl.accept_languages', user_language)
+        browser = webdriver.Firefox(options=options_firefox)
+    else:
+        raise pytest.UsageError("Выбери локализацию тестов: из ['es', 'en', 'fr', 'ru'] и выбранный браузер")
     yield browser
     print('\nЗакрытие браузера')
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,24 +1,9 @@
 import pytest
 import time
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-from conftest import language
-
-
-class TestElementDisplayed():
-    def test_items(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-        browser.get(link)
-        time.sleep(5)
-        try:
-            button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-            if button.is_displayed():
-                print("Кнопка отображается на странице.")
-            else:
-                print('Кнопка не отображается на странице')
-        except NoSuchElementException:
-            print('Кнопка не найдена')
-
-
-
+def test_items(browser):
+    link = 'http://selenium1py.pythonanywhere.com//catalogue/coders-at-work_207/'
+    browser.get(link)
+    # time.sleep(30)
+    assert browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket').is_displayed(), 'Кнопка не найдена на странице'
