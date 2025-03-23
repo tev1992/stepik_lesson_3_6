ac57260a0f4796097d50fc046f588d5acb6214f8
Trukhin_Evgeniy <tev1992@yandex.ru>
2025-03-23 17:03:02 +0500

    правки

--- conftest.py
+++ conftest.py
@@ -5,13 +5,15 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None, help='Выбери локализацию тестов: "--language"')
-    parser.addoption('--browser_name', action='store', default="Chrome", help='Выбери браузер: "--browser_name=chrome" or "--browser_name=firefox"')
+    parser.addoption('--browser_name', action='store', default='Chrome', help='Выбери браузер: "--browser_name=chrome" or "--browser_name=firefox"')
 
 @pytest.fixture(scope='function')
 def browser(request):
     user_language = request.config.getoption('language')
     browser_name = request.config.getoption('browser_name')
-
+    # Проверка, что язык указан при запуске тестов
+    if user_language is None:
+        raise pytest.UsageError("Выбери локализацию тестов, пример: ['--language=es', '--language=en', '--language=fr', '--language=ru']")
     # Настройка браузера
     if browser_name == 'Chrome':
         options = Options()
@@ -21,8 +23,9 @@ def browser(request):
         options_firefox = OptionsFirefox()
         options_firefox.set_preference('intl.accept_languages', user_language)
         browser = webdriver.Firefox(options=options_firefox)
+    # Проверка, если браузер не задан, по умолчанию открывается 'Chrome'
     else:
-        raise pytest.UsageError("Выбери локализацию тестов: из ['es', 'en', 'fr', 'ru'] и выбранный браузер")
+        raise pytest.UsageError("выбери браузер: 'Сhrome' или 'Firefox'")
     yield browser
     print('\nЗакрытие браузера')
     browser.quit()
