8351b53579fc832e4e1afaedecaabb73876837f8
Trukhin_Evgeniy <tev1992@yandex.ru>
2025-03-23 17:03:59 +0500

    правки

--- test_items.py
+++ test_items.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 def test_add_to_card_button_id_displayed(browser):
     link = 'http://selenium1py.pythonanywhere.com//catalogue/coders-at-work_207/'
     browser.get(link)
-    time.sleep(5)
     try:
         button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
     except NoSuchElementException:
